260b69d20994cad054e9068c484dbaa7b7ff210f
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-01-30 12:42:54 +0200

    Add view for wikipage

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+from markdown2 import markdown
 
 from . import util
 
@@ -8,3 +10,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wikipage(request, title):
+    page_markdown = util.get_entry(title)
+
+    if not page_markdown:
+        raise Http404(f'Wikipage for title "{title}" was not found')
+
+    return render(request, "encyclopedia/wikipage.html", {
+        "html": markdown(util.get_entry(title)),
+        "title": title
+    })
