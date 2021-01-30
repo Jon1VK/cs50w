a1dcba73119b7f88b8edc82eea84d23a18a61afe
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-01-30 12:20:33 +0200

    Add custom 404 error page

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["encyclopedia/templates/encyclopedia"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
