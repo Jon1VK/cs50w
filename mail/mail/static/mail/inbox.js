b7fa357b788dbfe3819725dcdc3d49b919dd9f6a
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-02-09 18:13:09 +0200

    Implement sending emails and showing mailboxes

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Use compose form submit to send the email
+  document.querySelector('#compose-form').addEventListener('submit', event => {
+    event.preventDefault();
+    send_email();
+  });
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +36,59 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      emails.forEach(email => {
+        const emailEl = document.createElement('div');
+        const senderEl = document.createElement('strong');
+        const subjectEl = document.createElement('span');
+        const timestampEl = document.createElement('time');
+
+        emailEl.classList.add('list-group-item')
+
+        emailEl.appendChild(senderEl);
+        emailEl.appendChild(subjectEl);
+        emailEl.appendChild(timestampEl);
+
+        senderEl.innerHTML = email.sender;
+        subjectEl.innerHTML = email.subject;
+        timestampEl.innerHTML = email.timestamp;
+
+        document.querySelector('#emails-view').appendChild(emailEl);
+      });
+  });
+
+}
+
+function send_email() {
+
+  // Get recipients, subject and body of an email form compose form input fields
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Send a POST request to API to send the email
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients,
+        subject,
+        body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // If the email was successfully sent, load the sent mailbox
+      if (result.message) {
+        load_mailbox('sent');
+      // Else alert the user with error message from API
+      } else {
+        alert(result.error);
+      }
+  });
 }
